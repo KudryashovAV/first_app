ba0ada9c3e205ff12c117e7659f2595817ad6db9
Alexey Kudryashov <akd@krononsoft.com>
2024-10-25 09:47:29 +0300

    First commit

---
+++ lib/gradient_container.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/styled_text.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(196, 80, 17, 135),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: StyledText(),
+      ),
+    );
+  }
+}
